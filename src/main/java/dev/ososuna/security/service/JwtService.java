0ffd5f00c6a490598a96df9c7ce05324cbdfe969
oswi <oswaldo.adrian@live.com.mx>
2023-01-04 18:34:37 -0700

    jwt methods

--- src/main/java/dev/ososuna/security/service/JwtService.java
+++ src/main/java/dev/ososuna/security/service/JwtService.java
@@ -1,12 +1,16 @@
 package dev.ososuna.security.service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.Map;
 import java.util.function.Function;
 
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 
@@ -16,12 +20,40 @@ public class JwtService {
   private static final String SECRET_KEY = "77397A244326462948404D635166546A576E5A7234753778214125442A472D4B";
 
   public String extractUsername(String token) {
-    return null;
+    return extractClaim(token, Claims::getSubject);
   }
 
   public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
     final Claims claims = extractAllClaims(token);
     return claimsResolver.apply(claims);
+  }
+
+  public String generateToken(UserDetails userDetails) {
+    return generateToken(Map.of(), userDetails);
+  }
+
+  public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails) {
+    return Jwts
+      .builder()
+      .setClaims(extraClaims)
+      .setSubject(userDetails.getUsername())
+      .setIssuedAt(new Date(System.currentTimeMillis()))
+      .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24)) // 1 day
+      .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+      .compact();
+  }
+
+  public boolean isTokenValid(String token, UserDetails userDetails) {
+    final String username = extractUsername(token);
+    return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+  }
+
+  private boolean isTokenExpired(String token) {
+    return extractExpiration(token).before(new Date());
+  }
+
+  private Date extractExpiration(String token) {
+    return extractClaim(token, Claims::getExpiration);
   }
 
   private Claims extractAllClaims(String token) {
