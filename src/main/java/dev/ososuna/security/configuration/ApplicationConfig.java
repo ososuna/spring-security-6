5bf6cd4d5c94f958402d27d9e431b06699f36d39
oswi <oswaldo.adrian@live.com.mx>
2023-01-04 19:16:16 -0700

    authentication provider

--- src/main/java/dev/ososuna/security/configuration/ApplicationConfig.java
+++ src/main/java/dev/ososuna/security/configuration/ApplicationConfig.java
@@ -2,8 +2,12 @@ package dev.ososuna.security.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import dev.ososuna.security.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -20,4 +24,16 @@ public class ApplicationConfig {
       .orElseThrow(() -> new UsernameNotFoundException("User not found"));
   }
 
+  @Bean
+  AuthenticationProvider authenticationProvider() {
+    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+    authProvider.setUserDetailsService(userDetailsService());
+    authProvider.setPasswordEncoder(passwordEncoder());
+    return authProvider;
+  }
+
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
 }
